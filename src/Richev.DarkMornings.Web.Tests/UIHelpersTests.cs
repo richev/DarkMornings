8c8ee4b41de6455ec81d2b7177e793d891d7c6d7
Richard Everett <richard@codrie.com>
2013-12-30 14:06:11 +0000

    Tests added

---
+++ src/Richev.DarkMornings.Web.Tests/UIHelpersTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using Richev.DarkMornings.Web.Models;
+
+namespace Richev.DarkMornings.Web.Tests
+{
+    [TestFixture]
+    public class UIHelpersTests
+    {
+        private DaylightInfo _daylightInfo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _daylightInfo = new DaylightInfo();
+        }
+
+        [Test]
+        public void GetJourneyTextShouldReturnCorrectTextInTypicalScenario()
+        {
+            _daylightInfo.NextWorkingDayDaylightTransition = new DateTime(2014, 3, 1);
+            _daylightInfo.NumberOfDaysToTransition = 10;
+
+            var journeyText = UIHelpers.GetJourneyText(_daylightInfo);
+
+            Assert.AreEqual("<p>You have <strong>10 more</strong> dark journeys to work, until <strong>Saturday 1 March</strong>.</p>", journeyText.ToString());
+        }
+
+        [Test]
+        public void GetJOurneyTextShouldReturnCorrectTextInEdgeCase()
+        {
+            _daylightInfo.IsCurrentlyInDaylight = true;
+
+            var journeyText = UIHelpers.GetJourneyText(_daylightInfo);
+
+            Assert.AreEqual("<p>Your journey to work is always in the <strong>light</strong>.</p>", journeyText.ToString());
+        }
+    }
+}
