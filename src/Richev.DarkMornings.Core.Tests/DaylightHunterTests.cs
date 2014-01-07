4e8d1d96086cd0f9e2297bdac91f885268e75a65
Richard Everett <richard@codrie.com>
2014-01-07 09:29:03 +0000

    Unit tests reworked to be data-driven.

--- src/Richev.DarkMornings.Core.Tests/DaylightHunterTests.cs
+++ src/Richev.DarkMornings.Core.Tests/DaylightHunterTests.cs
@@ -6,13 +6,13 @@ namespace Richev.DarkMornings.Core.Tests
     [TestFixture]
     public class DaylightHunterTests
     {
-        private readonly Location _locationLondon = new Location { Latitude = 51, Longitude = -3 };
+        private static readonly Location _locationLondon = new Location { Latitude = 51, Longitude = -3 };
         private const int TimeZoneLondon = 0;
 
-        private readonly Location _locationNewYork = new Location { Latitude = 40.67, Longitude = -73.94 };
+        private static readonly Location _locationNewYork = new Location { Latitude = 40.67, Longitude = -73.94 };
         private const int TimeZoneNewYork = -5;
 
-        private readonly Location _locationSydney = new Location { Latitude = -33.859972, Longitude = 151.211111 };
+        private static readonly Location _locationSydney = new Location { Latitude = -33.859972, Longitude = 151.211111 };
         private const int TimeZoneSydney = 10;
 
         private DaylightHunter _daylightHunter;
@@ -23,123 +23,45 @@ namespace Richev.DarkMornings.Core.Tests
             _daylightHunter = new DaylightHunter();
         }
 
-        [Test]
-        public void IsCorrectInWinter()
+        private static readonly object[] _isCorrectForScenarioCases =
         {
-            var outboundCommuteStart = new DateTime(2013, 12, 24, 7, 10, 0);
-            var returnCommuteStart = new DateTime(2013, 12, 24, 18, 0, 0);
+            new object[] { "London in the winter", _locationLondon, TimeZoneLondon, new DateTime(2013, 12, 24, 7, 10, 0), new DateTime(2013, 12, 24, 18, 0, 0), false, new DateTime(2014, 02, 27, 7, 10, 0), DaylightTransition.SunRise, false, new DateTime(2014, 03, 29, 18, 31, 0), DaylightTransition.SunSet },
+            
+            new object[] { "London in the summer", _locationLondon, TimeZoneLondon, new DateTime(2014, 6, 24, 7, 10, 0), new DateTime(2014, 6, 24, 18, 0, 0), true, new DateTime(2014, 09, 24, 7, 11, 0), DaylightTransition.SunRise, true, new DateTime(2014, 10, 7, 18, 29, 0), DaylightTransition.SunSet },
+            
+            new object[] { "Sydney in the winter", _locationSydney, TimeZoneSydney, new DateTime(2014, 6, 24, 6, 10, 0), new DateTime(2014, 6, 24, 18, 0, 0), false, new DateTime(2014, 9, 5, 6, 10, 0), DaylightTransition.SunRise, false, new DateTime(2014, 10, 06, 18, 59, 0), DaylightTransition.SunSet },
+            
+            new object[] { "New York in the winter", _locationNewYork, TimeZoneNewYork, new DateTime(2014, 2, 24, 6, 30, 0), new DateTime(2014, 2, 24, 18, 30, 0), false, new DateTime(2014, 3, 6, 6, 30, 0), DaylightTransition.SunRise, false, new DateTime(2014, 3, 19, 19, 00, 0), DaylightTransition.SunSet },
+            
+            new object[] { "Always light or dark", _locationLondon, TimeZoneLondon, new DateTime(2014, 6, 24, 0, 10, 0), new DateTime(2014, 6, 24, 12, 0, 0), false, null, null, true, null, null },
+            
+            new object[] { "Always dark", _locationLondon, TimeZoneLondon, new DateTime(2014, 6, 24, 0, 10, 0), new DateTime(2014, 6, 24, 3, 0, 0), false, null, null, false, null, null },
+            
+            new object[] { "Always dark midnight rollover", _locationLondon, TimeZoneLondon, new DateTime(2014, 6, 24, 0, 10, 0), new DateTime(2014, 6, 24, 23, 0, 0), false, null, null, false, null, null },
 
-            var commuteInfo = _daylightHunter.GetDaylight(_locationLondon, TimeZoneLondon, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
-
-            Assert.IsFalse(commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 02, 27, 7, 10, 0), commuteInfo.ToWork.NextDaylightTransition);
-            Assert.AreEqual(DaylightTransition.SunRise, commuteInfo.ToWork.TransitionType);
-
-            Assert.IsFalse(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 03, 29, 18, 31, 0), commuteInfo.FromWork.NextDaylightTransition);
-            Assert.AreEqual(DaylightTransition.SunSet, commuteInfo.FromWork.TransitionType);
-        }
-
-        [Test]
-        public void IsCorrectInSummer()
-        {
-            var outboundCommuteStart = new DateTime(2014, 6, 24, 7, 10, 0);
-            var returnCommuteStart = new DateTime(2014, 6, 24, 18, 0, 0);
-
-            var commuteInfo = _daylightHunter.GetDaylight(_locationLondon, TimeZoneLondon, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
-
-            Assert.IsTrue(commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 09, 24, 7, 11, 0), commuteInfo.ToWork.NextDaylightTransition);
-
-            Assert.IsTrue(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 10, 7, 18, 29, 0), commuteInfo.FromWork.NextDaylightTransition);
-        }
+            new object[] { "Always light", _locationLondon, TimeZoneLondon, new DateTime(2014, 6, 24, 12, 0, 0), new DateTime(014, 6, 24, 13, 0, 0), true, null, null, true, null, null },
+        };
 
         [Test]
-        public void IsCorrectInSydneyWinter()
+        [TestCaseSource("_isCorrectForScenarioCases")]
+        public void IsCorrectForScenario(
+            string scenarioName,
+            Location location, double timeZone,
+            DateTime commuteToWorkStart, DateTime commuteToHomeStart,
+            bool toWorkIsCurrentlyInDaylight, DateTime? toWorkNextDaylightTransition, DaylightTransition? toWorkDaylightTransition,
+            bool fromWorkIsCurrentlyInDaylight, DateTime? fromWorkNextDaylightTransition, DaylightTransition? fromWorkDaylightTransition)
         {
-            var outboundCommuteStart = new DateTime(2014, 6, 24, 6, 10, 0);
-            var returnCommuteStart = new DateTime(2014, 6, 24, 18, 0, 0);
+            Console.WriteLine(scenarioName);
 
-            var commuteInfo = _daylightHunter.GetDaylight(_locationSydney, TimeZoneSydney, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
+            var commuteInfo = _daylightHunter.GetDaylight(location, timeZone, commuteToWorkStart, commuteToWorkStart.AddMinutes(30), commuteToHomeStart, commuteToHomeStart.AddMinutes(30));
 
-            Assert.IsFalse(commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 9, 5, 6, 10, 0), commuteInfo.ToWork.NextDaylightTransition);
+            Assert.AreEqual(toWorkIsCurrentlyInDaylight, commuteInfo.ToWork.IsCurrentlyInDaylight);
+            Assert.AreEqual(toWorkNextDaylightTransition, commuteInfo.ToWork.NextDaylightTransition);
+            Assert.AreEqual(toWorkDaylightTransition, commuteInfo.ToWork.TransitionType);
 
-            Assert.IsFalse(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 10, 06, 18, 59, 0), commuteInfo.FromWork.NextDaylightTransition); // start of summer time
-        }
-
-        [Test]
-        public void IsCorrectForAlwaysDarkOrLightCommute()
-        {
-            var outboundCommuteStart = new DateTime(2014, 6, 24, 0, 10, 0);
-            var returnCommuteStart = new DateTime(2014, 6, 24, 12, 0, 0);
-
-            var commuteInfo = _daylightHunter.GetDaylight(_locationLondon, TimeZoneLondon, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
-
-            Assert.AreEqual(false, commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.ToWork.NextDaylightTransition);
-
-            Assert.AreEqual(true, commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.FromWork.NextDaylightTransition);
-        }
-
-        [Test]
-        public void IsCorrectForAlwaysDarkCommutes()
-        {
-            var outboundCommuteStart = new DateTime(2014, 6, 24, 0, 10, 0);
-            var returnCommuteStart = new DateTime(2014, 6, 24, 3, 0, 0);
-
-            var commuteInfo = _daylightHunter.GetDaylight(_locationLondon, TimeZoneLondon, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
-
-            Assert.IsFalse(commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.ToWork.NextDaylightTransition);
-
-            Assert.IsFalse(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.FromWork.NextDaylightTransition);
-        }
-
-        [Test]
-        public void IsCorrectForAlwaysDarkCommuteThatRollsOverMidnight()
-        {
-            var outboundCommuteStart = new DateTime(2014, 6, 24, 0, 10, 0);
-            var returnCommuteStart = new DateTime(2014, 6, 24, 23, 0, 0);
-
-            var commuteInfo = _daylightHunter.GetDaylight(_locationLondon, TimeZoneLondon, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(120));
-
-            Assert.IsFalse(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.FromWork.NextDaylightTransition);
-        }
-
-        [Test]
-        public void IsCorrectForAlwaysLightCommutes()
-        {
-            var outboundCommuteStart = new DateTime(2014, 6, 24, 12, 0, 0);
-            var returnCommuteStart = new DateTime(2014, 6, 24, 13, 0, 0);
-
-            var commuteInfo = _daylightHunter.GetDaylight(_locationLondon, TimeZoneLondon, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
-
-            Assert.IsTrue(commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.ToWork.NextDaylightTransition);
-
-            Assert.IsTrue(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(null, commuteInfo.FromWork.NextDaylightTransition);
-        }
-
-        [Test]
-        public void IsCorrectInNewYorkWinter()
-        {
-            var outboundCommuteStart = new DateTime(2014, 2, 24, 6, 30, 0);
-            var returnCommuteStart = new DateTime(2014, 2, 24, 18, 30, 0);
-
-            var commuteInfo = _daylightHunter.GetDaylight(_locationNewYork, TimeZoneNewYork, outboundCommuteStart, outboundCommuteStart.AddMinutes(30), returnCommuteStart, returnCommuteStart.AddMinutes(30));
-
-            Assert.IsFalse(commuteInfo.ToWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 3, 6, 6, 30, 0), commuteInfo.ToWork.NextDaylightTransition);
-
-            Assert.IsFalse(commuteInfo.FromWork.IsCurrentlyInDaylight);
-            Assert.AreEqual(new DateTime(2014, 3, 19, 19, 00, 0), commuteInfo.FromWork.NextDaylightTransition);
+            Assert.AreEqual(fromWorkIsCurrentlyInDaylight, commuteInfo.FromWork.IsCurrentlyInDaylight);
+            Assert.AreEqual(fromWorkNextDaylightTransition, commuteInfo.FromWork.NextDaylightTransition);
+            Assert.AreEqual(fromWorkDaylightTransition, commuteInfo.FromWork.TransitionType);
         }
     }
 }
