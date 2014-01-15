cf833f229e581ba9ad3cb9bc11e7a3ab470ac9fb
Richard Everett <richard@codrie.com>
2014-01-15 17:55:13 +0100

    Improved Tweet text

--- src/Richev.DarkMornings.Web.Tests/UIHelpersTests.cs
+++ src/Richev.DarkMornings.Web.Tests/UIHelpersTests.cs
@@ -85,5 +85,163 @@ namespace Richev.DarkMornings.Web.Tests
 
             Assert.AreEqual("Monday, Tuesday and Thursday", formattedWorkingDays);
         }
+
+        [Test]
+        public void GetTweetTextShouldWorkForGeneralScenario()
+        {
+            var model = new CommuteInfoModel
+                            {
+                                tw = new CommuteTimeModel
+                                         {
+                                             Daylights = new DaylightInfoModel
+                                                             {
+                                                                 IsCurrentlyInDaylight = false,
+                                                                 NumberOfDaysToTransition = 13,
+                                                                 CommuteType = Commute.ToWork,
+                                                                 NextWorkingDayDaylightTransition = new DateTime(2014, 2, 1)
+                                                             }
+                                         },
+                                fw = new CommuteTimeModel
+                                         {
+                                             Daylights = new DaylightInfoModel
+                                                             {
+                                                                 IsCurrentlyInDaylight = true,
+                                                                 NumberOfDaysToTransition = 54,
+                                                                 CommuteType = Commute.FromWork,
+                                                                 NextWorkingDayDaylightTransition = new DateTime(2014, 2, 1)
+                                                             }
+                                         }
+                            };
+
+            var tweetText = UIHelpers.GetTweetText(model);
+
+            Assert.AreEqual(
+                "I have 13 more dark journeys to work and 54 more light journeys back home!",
+                tweetText);
+        }
+
+        [Test]
+        public void GetTweetTextShouldWorkWhenJourneyToWorkAlwaysInDark()
+        {
+            var model = new CommuteInfoModel
+            {
+                tw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = false,
+                        CommuteType = Commute.ToWork
+                    }
+                },
+                fw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = true,
+                        NumberOfDaysToTransition = 54,
+                        CommuteType = Commute.FromWork,
+                        NextWorkingDayDaylightTransition = new DateTime(2014, 2, 1)
+                    }
+                }
+            };
+
+            var tweetText = UIHelpers.GetTweetText(model);
+
+            Assert.AreEqual(
+                "My journey to work is always in the dark and I have 54 more light journeys back home!",
+                tweetText);
+        }
+
+        [Test]
+        public void GetTweetTextShouldWorkWhenJourneyFromWorkAlwaysInLight()
+        {
+            var model = new CommuteInfoModel
+            {
+                tw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = false,
+                        CommuteType = Commute.ToWork,
+                        NumberOfDaysToTransition = 54,
+                        NextWorkingDayDaylightTransition = new DateTime(2014, 2, 1)
+                    }
+                },
+                fw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = true,
+                        CommuteType = Commute.FromWork,
+                    }
+                }
+            };
+
+            var tweetText = UIHelpers.GetTweetText(model);
+            
+            Assert.AreEqual(
+                "I have 54 more dark journeys to work and my journey back home is always in the light!",
+                tweetText);
+        }
+
+        [Test]
+        public void GetTweetTextShouldWorkWhenJourneyToWorkAlwaysInDarkAndFromWorkAlwaysInLight()
+        {
+            var model = new CommuteInfoModel
+            {
+                tw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = false,
+                        CommuteType = Commute.ToWork,
+                    }
+                },
+                fw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = true,
+                        CommuteType = Commute.FromWork,
+                    }
+                }
+            };
+
+            var tweetText = UIHelpers.GetTweetText(model);
+
+            Assert.AreEqual(
+                "My journey to work is always in the dark and my journey back home is always in the light!",
+                tweetText);
+        }
+
+        [Test]
+        public void GetTweetTextShouldWorkWhenJourneyToWorkAlwaysInDarkAndFromWorkAlwaysInDark()
+        {
+            var model = new CommuteInfoModel
+            {
+                tw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = false,
+                        CommuteType = Commute.ToWork,
+                    }
+                },
+                fw = new CommuteTimeModel
+                {
+                    Daylights = new DaylightInfoModel
+                    {
+                        IsCurrentlyInDaylight = false,
+                        CommuteType = Commute.FromWork,
+                    }
+                }
+            };
+
+            var tweetText = UIHelpers.GetTweetText(model);
+
+            Assert.AreEqual(
+                "My journeys to work and back home are always in the dark!",
+                tweetText);
+        }
     }
 }
--- src/Richev.DarkMornings.Web/UIHelpers.cs
+++ src/Richev.DarkMornings.Web/UIHelpers.cs
@@ -56,6 +56,7 @@ namespace Richev.DarkMornings.Web
             var jds = new List<KeyValuePair<string, int>> 
                      {
                          new KeyValuePair<string, int>("5 minutes", 5),
+                         new KeyValuePair<string, int>("10 minutes", 10),
                          new KeyValuePair<string, int>("15 minutes", 15),
                          new KeyValuePair<string, int>("30 minutes", 30),
                          new KeyValuePair<string, int>("45 minutes", 45),
@@ -100,12 +101,17 @@ namespace Richev.DarkMornings.Web
     <span class=""date"">{5:%d}</span>
     <span class=""month"">{5:MMMM}</span>
 </div>",
+/*<div class=""percent"">
+    <span>In the dark</span>{6:0.#}%<span>of the time!</span>
+</div>*/
+
                     GetDaylightIcon(daylightInfo.IsCurrentlyInDaylight),
                     GetCommuteDirectionIcon(daylightInfo.CommuteType),
                     daylightInfo.NumberOfDaysToTransition,
                     GetDaylightText(daylightInfo.IsCurrentlyInDaylight),
                     GetCommuteText(daylightInfo.CommuteType),
-                    daylightInfo.NextWorkingDayDaylightTransition.Value);
+                    daylightInfo.NextWorkingDayDaylightTransition.Value,
+                    daylightInfo.PercentageInDaylight);
             }
             else
             {
@@ -216,14 +222,49 @@ namespace Richev.DarkMornings.Web
 
         public static string GetTweetText(CommuteInfoModel model)
         {
-            var tweetText = string.Format(
-                "I have {0} more {1} journeys to work and {2} more {3} journeys from work!",
-                model.tw.Daylights.NumberOfDaysToTransition,
-                GetDaylightText(model.tw.Daylights.IsCurrentlyInDaylight),
-                model.fw.Daylights.NumberOfDaysToTransition,
-                GetDaylightText(model.fw.Daylights.IsCurrentlyInDaylight));
+            string tweetText;
+
+            if (!model.tw.Daylights.NextWorkingDayDaylightTransition.HasValue &&
+                !model.fw.Daylights.NextWorkingDayDaylightTransition.HasValue &&
+                model.tw.Daylights.IsCurrentlyInDaylight == model.fw.Daylights.IsCurrentlyInDaylight)
+            {
+                tweetText = string.Format(
+                    "My journeys {0} and {1} are always in the {2}!",
+                    GetCommuteText(model.tw.Daylights.CommuteType),
+                    GetCommuteText(model.fw.Daylights.CommuteType),
+                    GetDaylightText(model.tw.Daylights.IsCurrentlyInDaylight));
+            }
+            else
+            {
+                var prefixToWorkWithIHave = model.tw.Daylights.NextWorkingDayDaylightTransition.HasValue;
+                var prefixFromWorkWithIHave = !prefixToWorkWithIHave && model.fw.Daylights.NextWorkingDayDaylightTransition.HasValue;
+
+                tweetText = string.Format(
+                    "{0} and {1}!",
+                    GetDaylightInfoTweetText(model.tw.Daylights, prefixToWorkWithIHave),
+                    GetDaylightInfoTweetText(model.fw.Daylights, prefixFromWorkWithIHave));
+            }
 
             return tweetText;
+        }
+
+        private static string GetDaylightInfoTweetText(DaylightInfoModel daylightInfo, bool prefixWithIHave)
+        {
+            if (daylightInfo.NextWorkingDayDaylightTransition.HasValue)
+            {
+                return string.Format(
+                    "{0}{1} more {2} journeys {3}",
+                    prefixWithIHave ? "I have " : string.Empty,
+                    daylightInfo.NumberOfDaysToTransition,
+                    GetDaylightText(daylightInfo.IsCurrentlyInDaylight),
+                    GetCommuteText(daylightInfo.CommuteType));
+            }
+
+            return string.Format(
+                "{0} journey {1} is always in the {2}",
+                daylightInfo.CommuteType == Commute.ToWork ? "My" : "my",
+                GetCommuteText(daylightInfo.CommuteType),
+                GetDaylightText(daylightInfo.IsCurrentlyInDaylight));
         }
     }
 }
