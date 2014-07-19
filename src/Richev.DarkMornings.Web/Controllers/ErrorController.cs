bee0bbe2b2d8e88a26a62af89f89517fdbfd9cd3
Richard Everett <richard@codrie.com>
2014-07-19 17:19:20 +0100

    Error pages added.

---
+++ src/Richev.DarkMornings.Web/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Richev.DarkMornings.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult InternalError()
+        {
+            return View();
+        }
+    }
+}
