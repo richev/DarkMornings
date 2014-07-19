3ee075cf164c2a96cfd84265bc95000dc3fe2639
Richard Everett <richard@codrie.com>
2014-07-19 18:53:00 +0100

    A more correct approach to HTTP error handling.

--- src/Richev.DarkMornings.Web/Controllers/ErrorController.cs
+++ src/Richev.DarkMornings.Web/Controllers/ErrorController.cs
@@ -7,12 +7,14 @@ namespace Richev.DarkMornings.Web.Controllers
         [HttpGet]
         public ActionResult NotFound()
         {
+            Response.StatusCode = (int)System.Net.HttpStatusCode.NotFound;
             return View();
         }
 
         [HttpGet]
         public ActionResult InternalError()
         {
+            Response.StatusCode = (int)System.Net.HttpStatusCode.InternalServerError;
             return View();
         }
     }
