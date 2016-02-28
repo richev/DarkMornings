d588c0c54eee10b7058e87b800ebfe7b097378b1
Richard Everett <richard@codrie.com>
2016-02-28 11:44:15 +0000

    X- headers removed

--- src/Richev.DarkMornings.Web/Global.asax.cs
+++ src/Richev.DarkMornings.Web/Global.asax.cs
@@ -12,6 +12,7 @@ namespace Richev.DarkMornings.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MvcHandler.DisableMvcResponseHeader = true;
         }
     }
 }
